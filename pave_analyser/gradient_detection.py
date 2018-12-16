Fixed bug in edge generation during gradient detection.

pave_analyser/gradient_detection.py
@@ -14,22 +14,20 @@ def detect_high_gradient_pixels(temperatures, offsets, tolerance, diagonal_adjac
     """
     gradient_map = np.zeros(temperatures.shape, dtype='bool')
     edges = []
+    add_edges = lambda x:edges.append(x)
 
     for idx in range(len(offsets) - 1):
         # Locate the temperature gradients in the driving direction
-        longitudinal_edges = _detect_longitudinal_gradients(idx, offsets, temperatures, gradient_map, tolerance)
-        edges.append(longitudinal_edges)
+        add_edges(_detect_longitudinal_gradients(idx, offsets, temperatures, gradient_map, tolerance))
 
         # Locate the temperature gradients in the transversal direction
-        tranversal_edges = _detect_transversal_gradients(idx, offsets, temperatures, gradient_map, tolerance)
-        edges.append(tranversal_edges)
+        add_edges(_detect_transversal_gradients(idx, offsets, temperatures, gradient_map, tolerance))
 
         if diagonal_adjacency:
-            _detect_diagonal_gradients_right(idx, offsets, temperatures, gradient_map, tolerance)
-            _detect_diagonal_gradients_left(idx, offsets, temperatures, gradient_map, tolerance)
+            add_edges(_detect_diagonal_gradients_right(idx, offsets, temperatures, gradient_map, tolerance))
+            add_edges(_detect_diagonal_gradients_left(idx, offsets, temperatures, gradient_map, tolerance))
 
-    tranversal_edges = _detect_transversal_gradients(idx + 1, offsets, temperatures, gradient_map, tolerance)
-    edges.append(tranversal_edges)
+    add_edges(_detect_transversal_gradients(idx + 1, offsets, temperatures, gradient_map, tolerance))
 
     gradient_graph = _create_gradient_graph(edges)
     clusters = list(_extract_clusters(gradient_graph))
@@ -98,10 +96,11 @@ def _detect_diagonal_gradients_right(idx, offsets, temperatures, gradient_map, t
     temperature_slice_next = temperatures[idx + 1, next_start:next_end]
 
     (indices, ) = np.where(np.abs(temperature_slice - temperature_slice_next) > tolerance)
+
     gradient_map[idx, start:end][indices] = 1
     gradient_map[idx + 1, next_start:next_end][indices] = 1
 
-    edges = _calc_edges(idx, indices, idx + 1, indices + 1)
+    edges = _calc_edges(idx, indices + start, idx + 1, indices + next_start)
     return edges
 
 
@@ -144,7 +143,7 @@ def _detect_diagonal_gradients_left(idx, offsets, temperatures, gradient_map, to
     gradient_map[idx, start:end][indices] = 1
     gradient_map[idx + 1, next_start:next_end][indices] = 1
 
-    edges = _calc_edges(idx, indices + 1, idx + 1, indices)
+    edges = _calc_edges(idx, indices + start, idx + 1, indices + next_start)
     return edges
 
 

pave_analyser/gradient_detection_test.py
@@ -280,11 +280,15 @@ class TestClusterExtraction(unittest.TestCase):
                 [1, 0, 0],
                 [0, 0, 0],
                 ])
-        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance)
-        largest_cluster = set(clusters[0])
+        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=False)
+        _, clusters_diag = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=True)
         self.assertEqual(
-                largest_cluster,
+                set(clusters[0]),
                 {(0, 0), (1, 0), (1,1), (2, 0)}
+                )
+        self.assertEqual(
+                set(clusters_diag[0]),
+                {(0, 0), (0, 1), (1, 0), (1,1), (2, 0), (2, 1)}
                 )
 
 
@@ -294,11 +298,15 @@ class TestClusterExtraction(unittest.TestCase):
                 [0, 0, 0],
                 [0, 0, 1],
                 ])
-        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance)
-        largest_cluster = set(clusters[0])
+        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=False)
+        _, clusters_diag = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=True)
         self.assertEqual(
-                largest_cluster,
+                set(clusters[0]),
                 {(1, 2), (2, 1), (2, 2)}
+                )
+        self.assertEqual(
+                set(clusters_diag[0]),
+                {(1,1), (1, 2), (2, 1), (2, 2)}
                 )
 
 
@@ -308,21 +316,27 @@ class TestClusterExtraction(unittest.TestCase):
                 [0, 1, 0],
                 [0, 0, 0],
                 ])
-        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance)
-        largest_cluster = set(clusters[0])
+        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=False)
+        _, clusters_diag = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=True)
         self.assertEqual(
-                largest_cluster,
+                set(clusters[0]),
                 {(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)}
+                )
+        self.assertEqual(
+                set(clusters_diag[0]),
+                {(0, 0), (0, 1), (0, 2),
+                 (1, 0), (1, 1), (1, 2),
+                 (2, 0), (2, 1), (2, 2)}
                 )
 
 
-    def test_clustering_two_clusters(self):
+    def test_clustering_two_clusters_no_diagonal(self):
         t = np.array([
                 [2, 1, 0],
                 [0, 0, 0],
                 [0, 0, 1],
                 ])
-        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance)
+        _, clusters = detect_high_gradient_pixels(t, self.offsets, self.tolerance, diagonal_adjacency=False)
         largest_cluster = set(clusters[0])
         second_largest_cluster = set(clusters[1])
         self.assertEqual(
@@ -333,6 +347,59 @@ class TestClusterExtraction(unittest.TestCase):
                 second_largest_cluster,
                 {(1, 2), (2, 1), (2, 2)}
                 )
+
+
+    def test_clustering_two_clusters_with_diagonal(self):
+        t = np.array([
+                [2, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 1],
+                ])
+        offsets = [[0, 3], [0, 3], [0, 3], [0, 3]]
+        _, clusters = detect_high_gradient_pixels(t, offsets, self.tolerance, diagonal_adjacency=True)
+        largest_cluster = set(clusters[0])
+        second_largest_cluster = set(clusters[1])
+        self.assertEqual(
+                largest_cluster,
+                {(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)}
+                )
+        self.assertEqual(
+                second_largest_cluster,
+                {(2, 1), (2, 2), (3, 1), (3, 2)}
+                )
+
+
+    def test_large_cluster(self):
+        from road_identification import estimate_road_length
+        threshold = 1.0
+        adjust_npixel = 0
+        tolerance = 0.1
+        pixels = np.array([
+            [0, 9, 8, 7, 9],
+            [9, 8, 7, 8, 0]
+            ])
+        offsets, road_pixels = estimate_road_length(pixels, threshold, adjust_npixel)
+        gradient_map, clusters = detect_high_gradient_pixels(pixels, offsets, tolerance, diagonal_adjacency=True)
+        self.assertEqual(offsets, [(1, 5), (0, 4)])
+        np.testing.assert_array_equal(
+                road_pixels,
+                np.array([
+                    [0, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 0]
+                    ])
+                )
+        np.testing.assert_array_equal(
+                gradient_map,
+                np.array([
+                    [0, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 0]
+                    ])
+                )
+        self.assertEqual(set(clusters[0]), {
+            (0, 1), (0, 2), (0, 3), (0, 4),
+            (1, 0), (1, 1), (1, 2), (1, 3)}
+            )
 
 
 if __name__ == '__main__':