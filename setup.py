f5cefda310900aaeed777fd74e6ec1eaf6736401
Lasse G. Andersen <lasse@etableret.dk>
2021-02-18 09:52:11 +0100

    preparing for pip packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='RoadTherma',
-      version='0.1',
+      version='0.0.2',
       description='Tool for analysing termal data measured during paving operations.',
       author='Lasse Grinderslev Andersen',
-      author_email='lasse@etableret.dk',
+      author_email='lasse@philomath.dk',
       url='https://github.com/roadtools/roadtherma',
       packages=['roadtherma'],
       entry_points = {
